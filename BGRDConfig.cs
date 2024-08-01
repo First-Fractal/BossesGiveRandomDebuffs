a9aff1a1736ddbffce25975bb3fe50bde789c8bf
First Fractal <firstfractal512@gmail.com>
2024-07-31 22:36:49 -0500

    added in a config option

---
+++ BGRDConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace BossesGiveRandomDebuffs
+{
+    internal class BGRDConfig : ModConfig
+    {
+        //tell the mod that the configs are on the server and logistical side of the mod. 
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        //define the instance for the mod
+        public static BGRDConfig Instance;
+
+        //display the general options line 
+        [Header("$Mods.RandomBuffsMod.Configs.GeneralOptions")]
+
+        //define the config value for boss minions giving debuffs
+        [DefaultValue(true)]
+        public bool BossMinions;
+
+        //define the config value for the chance of getting the debuff
+        [DefaultValue(50)]
+        [Slider()]
+        [Range(0, 100)]
+        public int DebuffChance;
+
+        //define the config value for boss minions giving debuffs
+        [DefaultValue(5)]
+        [Range(0, 99999)]
+        public int DebuffTimeMin;
+
+        //define the config value for boss minions giving debuffs
+        [DefaultValue(120)]
+        [Range(0, 99999)]
+        public int DebuffTimeMax;
+    }
+}
--- BGRDGlobalNPC.cs
+++ BGRDGlobalNPC.cs
@@ -10,20 +10,24 @@ namespace BossesGiveRandomDebuffs
     {
         public override void OnHitPlayer(NPC npc, Player target, Player.HurtInfo hurtInfo)
         {
-            //check if the current npc is a boss or a boss part or a boss minion
-            if (npc.boss || ffVar.Bosses.BossParts.Contains(npc.type) || ffVar.Bosses.BossMinions.Contains(npc.type))
+            //check if the current npc is a boss or a boss part or a boss minion if allowed
+            if (npc.boss || ffVar.Bosses.BossParts.Contains(npc.type) || (BGRDConfig.Instance.BossMinions && ffVar.Bosses.BossMinions.Contains(npc.type)))
             {
-                //get a random buff from the list
-                int randomBuffID = Main.rand.Next(1, BGRDSystem.debuffs.Count - 1);
+                //make it have a 50% chance to apply it
+                if (Main.rand.Next(0, 101) <= BGRDConfig.Instance.DebuffChance)
+                {
+                    //get a random buff from the list
+                    int randomBuffID = Main.rand.Next(1, BGRDSystem.debuffs.Count - 1);
 
-                //clamp the random buff id just in case
-                randomBuffID = Math.Clamp(randomBuffID, 1, BGRDSystem.debuffs.Count - 1);
+                    //clamp the random buff id just in case
+                    randomBuffID = Math.Clamp(randomBuffID, 1, BGRDSystem.debuffs.Count - 1);
 
-                //grab the random buff from the allowed list
-                randomBuffID = BGRDSystem.debuffs[randomBuffID];
+                    //grab the random buff from the allowed list
+                    randomBuffID = BGRDSystem.debuffs[randomBuffID];
 
-                //apply the buff
-                target.AddBuff(randomBuffID, Main.rand.Next(ffFunc.TimeToTick(5), ffFunc.TimeToTick(0, 2)));
+                    //apply the buff
+                    target.AddBuff(randomBuffID, Main.rand.Next(ffFunc.TimeToTick(BGRDConfig.Instance.DebuffTimeMin), ffFunc.TimeToTick(BGRDConfig.Instance.DebuffTimeMax)));
+                }
             }
 
             base.OnHitPlayer(npc, target, hurtInfo);
